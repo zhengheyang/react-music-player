13fb99e4952ed9746984e3afd46918eff27898b6
ROG\zhyang <zhenghe_yang@hotmail.com>
2017-09-13 23:18:04 +0800

    初始化工程，配置依赖，配置webpack

---
+++ app/components/hello.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./hello.less";
+
+export default class Hello extends Component {
+	render() {
+		return <div className="hello-component">Hello,React</div>;
+	}
+}
---
+++ app/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import Hello from "./components/hello";
+
+render(<Hello />, document.getElementById("root"));
